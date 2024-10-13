cebee236f01f96161b472f887e7f6cbf8a340699
Giovanni Chahin Morassi <giovannimorassi13511@gmail.com>
2024-10-13 19:25:35 -0300

    Printar no lcd
    estou tentando printar no lcd

--- code.asm
+++ code.asm
@@ -1,9 +1,25 @@
+; --- Mapeamento de Hardware (8051) ---
+    RS      equ     P1.3    ;Reg Select ligado em P1.3
+    EN      equ     P1.2    ;Enable ligado em P1.2
+
+Aberto:
+DB "Aberto"
+DB 0
+
+Errou:
+DB "Sennha incorreta"
+DB 0 
+
 org 0000h
 LJMP MAIN
 
+
+
 org 0080h
 MAIN:   
-    
+    ACALL lcd_init
+	MOV A, #06h
+	ACALL posicionaCursor
 	MOV 50h,#0
 	MOV 51h,#0
 	MOV 52h,#0
@@ -44,7 +60,8 @@ MAIN:
 	mov r1 , 53h
 	CJNE R1, #1h, SENHA_INCORRETA
 	
-
+	MOV DPTR,#Aberto ;DPTR = início da palavra FEI
+	ACALL escreveString
 	
 
 
@@ -70,45 +87,22 @@ MAIN:
 ;11001111B - E
 ;10010111B - N
 
-SENHA_CORRETA:
-	SETB P3.3 ; |
-	SETB P3.4 ; | enable display 3
-	MOV P1, #10000001B ; put pattern for 1 on display
-;	CALL delay
-	CLR P3.3 ; enable display 2
-	MOV P1, #11100111B ; put pattern for 2 on display
-	;CALL delay
-	CLR P3.4 ; |
-	SETB P3.3 ; | enable display 1
-	MOV P1, #11001111B ; put pattern for 3 on display
-	;CALL delay
-	CLR P3.3 ; enable display 0
-	MOV P1, #10010111B ; put pattern for 4 on display
-	;CALL delay
-	RET
 
 SENHA_INCORRETA:
-	SETB P3.3 ; |
-	SETB P3.4 ; | enable display 3
-	MOV P1, #10000001B ; put pattern for 1 on display
-	CALL delay
-	CLR P3.3 ; enable display 2
-	MOV P1, #11100111B ; put pattern for 2 on display
-	CALL delay
-	CLR P3.4 ; |
-	SETB P3.3 ; | enable display 1
-	MOV P1, #11001111B ; put pattern for 3 on display
-	CALL delay
-	CLR P3.3 ; enable display 0
-	MOV P1, #10010111B ; put pattern for 4 on display
-	CALL delay
+	MOV DPTR,#Errou ;DPTR = início da palavra FEI
+	ACALL escreveString
 	JMP MAIN
 
+escreveString:
+MOV R2, #0
+rot:
+MOV A, R2
+MOVC A,@A+DPTR ;lê a tabela da memória de programa
+ACALL sendCharacter ; send data in A to LCD module
+INC R2
+JNZ rot ; if A is 0, then end of data has been reached - jump out of loop
+RET
 
-delay:
-	MOV R0, #200
-	DJNZ R0, $	
-	RET
 
 
 ler:
@@ -157,3 +151,175 @@ RET ; return from subroutine - key not found
 gotKey:
 SETB F0 ; key found - set F0
 RET ; and return from subroutine
+
+
+sendCharacter :
+SETB RS ; set RS - indicates that data is being sent to module
+MOV C, ACC.7 ; |
+MOV P1.7, C ; |
+MOV C, ACC.6 ; |
+MOV P1.6, C ; |
+MOV C, ACC.5 ; |
+MOV P1.5, C ; |
+MOV C, ACC.4 ; |
+MOV P1.4, C ; | high nibble set
+SETB EN ; |
+CLR EN ; | neg
+lcd_init:
+
+	CLR RS		; clear RS - indicates that instructions are being sent to the module
+
+; function set	
+	CLR P1.7		; |
+	CLR P1.6		; |
+	SETB P1.5		; |
+	CLR P1.4		; | high nibble set
+
+	SETB EN		; |
+	CLR EN		; | negative edge on E
+
+	CALL delay		; wait for BF to clear	
+					; function set sent for first time - tells module to go into 4-bit mode
+; Why is function set high nibble sent twice? See 4-bit operation on pages 39 and 42 of HD44780.pdf.
+
+	SETB EN		; |
+	CLR EN		; | negative edge on E
+					; same function set high nibble sent a second time
+
+	SETB P1.7		; low nibble set (only P1.7 needed to be changed)
+
+	SETB EN		; |
+	CLR EN		; | negative edge on E
+				; function set low nibble sent
+	CALL delay		; wait for BF to clear
+
+
+; entry mode set
+; set to increment with no shift
+	CLR P1.7		; |
+	CLR P1.6		; |
+	CLR P1.5		; |
+	CLR P1.4		; | high nibble set
+
+	SETB EN		; |
+	CLR EN		; | negative edge on E
+
+	SETB P1.6		; |
+	SETB P1.5		; |low nibble set
+
+	SETB EN		; |
+	CLR EN		; | negative edge on E
+
+	CALL delay		; wait for BF to clear
+
+
+; display on/off control
+; the display is turned on, the cursor is turned on and blinking is turned on
+	CLR P1.7		; |
+	CLR P1.6		; |
+	CLR P1.5		; |
+	CLR P1.4		; | high nibble set
+
+	SETB EN		; |
+	CLR EN		; | negative edge on E
+
+	SETB P1.7		; |
+	SETB P1.6		; |
+	SETB P1.5		; |
+	SETB P1.4		; | low nibble set
+
+	SETB EN		; |
+	CLR EN		; | negative edge on E
+
+	CALL delay		; wait for BF to clear
+	RET
+
+
+;Retorna o cursor para primeira posi��o sem limpar o display
+retornaCursor:
+	CLR RS	      ; clear RS - indicates that instruction is being sent to module
+	CLR P1.7		; |
+	CLR P1.6		; |
+	CLR P1.5		; |
+	CLR P1.4		; | high nibble set
+
+	SETB EN		; |
+	CLR EN		; | negative edge on E
+
+	CLR P1.7		; |
+	CLR P1.6		; |
+	SETB P1.5		; |
+	SETB P1.4		; | low nibble set
+
+	SETB EN		; |
+	CLR EN		; | negative edge on E
+
+	CALL delay		; wait for BF to clear
+	RET
+
+
+;Limpa o display
+clearDisplay:
+	CLR RS	      ; clear RS - indicates that instruction is being sent to module
+	CLR P1.7		; |
+	CLR P1.6		; |
+	CLR P1.5		; |
+	CLR P1.4		; | high nibble set
+
+	SETB EN		; |
+	CLR EN		; | negative edge on E
+
+	CLR P1.7		; |
+	CLR P1.6		; |
+	CLR P1.5		; |
+	SETB P1.4		; | low nibble set
+
+	SETB EN		; |
+	CLR EN		; | negative edge on E
+	mov r7, #20
+	delayChamar:
+;	CALL delay		
+	djnz r7, delay
+
+	RET
+
+
+delay:
+	MOV R0, #50
+	DJNZ R0, $
+	RET
+
+;Posiciona o cursor na linha e coluna desejada.
+;Escreva no Acumulador o valor de endere�o da linha e coluna.
+;|--------------------------------------------------------------------------------------|
+;|linha 1 | 00 | 01 | 02 | 03 | 04 |05 | 06 | 07 | 08 | 09 |0A | 0B | 0C | 0D | 0E | 0F |
+;|linha 2 | 40 | 41 | 42 | 43 | 44 |45 | 46 | 47 | 48 | 49 |4A | 4B | 4C | 4D | 4E | 4F |
+;|--------------------------------------------------------------------------------------|
+posicionaCursor:
+	CLR RS	         ; clear RS - indicates that instruction is being sent to module
+	SETB P1.7		    ; |
+	MOV C, ACC.6		; |
+	MOV P1.6, C			; |
+	MOV C, ACC.5		; |
+	MOV P1.5, C			; |
+	MOV C, ACC.4		; |
+	MOV P1.4, C			; | high nibble set
+
+	SETB EN			; |
+	CLR EN			; | negative edge on E
+
+	MOV C, ACC.3		; |
+	MOV P1.7, C			; |
+	MOV C, ACC.2		; |
+	MOV P1.6, C			; |
+	MOV C, ACC.1		; |
+	MOV P1.5, C			; |
+	MOV C, ACC.0		; |
+	MOV P1.4, C			; | low nibble set
+
+	SETB EN			; |
+	CLR EN			; | negative edge on E
+
+	CALL delay			; wait for BF to clear
+	RET
+
